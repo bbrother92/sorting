57b43ddddc482053f543ae4cff83872b9ef8f795
bbrother92 <bbrohter92@gmail.com>
2016-10-12 17:39:01 +0300

    buble sort
    simplest sorting algorithm

---
+++ buble/buble.java
@@ -0,0 +1,32 @@
+/**
+ * Created by vi on 21.09.2016.
+ */
+import java.util.Arrays;
+public class buble {
+   static int tt;
+
+
+    public static void main(String[] args) {
+
+        int arrayList[] = { 5,3,9,7,1,8 };
+        System.out.println("Final result:"+Arrays.toString(ascend(arrayList)));
+        System.out.println(tt);
+
+    }
+
+    public static int[] ascend(int[] arr){
+        int temp;
+        for(int i=0; i < arr.length-1; i++){
+            tt = i;
+            for(int j=1; j < arr.length-i; j++){
+                if(arr[j-1] > arr[j]){
+                    temp=arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.println((i+1)+"th iteration result: "+Arrays.toString(arr));
+        }
+        return arr;
+    }
+}
